d5fbf5e2229c03ac4baf593dd1d384aae8cfcc55
tommeldrumk3btg <78870858+tommeldrumk3btg@users.noreply.github.com>
2021-08-03 21:51:50 +0100

    com

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Response } from '@angular/http';
 import { AlbumStorePage } from '../../e2e/app.po';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
 
-  private _albumUrl = '../assets/AlbumStorePage.json'
-
-  getAlbum() {
-    id: Number
-  }
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) {}
 
-  
+  getAlbum(id: Number) {
+    return this._http.get(this._albumUrl).map((response) =>
+    response.json())
+  }
 }
