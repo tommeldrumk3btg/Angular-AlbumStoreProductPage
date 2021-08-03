590e6937cd08e06f6b67bf6e7e35584a29aa83b5
tommeldrumk3btg <78870858+tommeldrumk3btg@users.noreply.github.com>
2021-08-03 18:15:31 +0100

    commit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+import { AlbumStorePage } from '../../e2e/app.po';
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  private _albumUrl = '../assets/AlbumStorePage.json'
 
+  getAlbum() {
+    id: Number
+  }
+
+  constructor(private _http: Http) {}
+
+  
 }
